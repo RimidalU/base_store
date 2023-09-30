2b3154cf21222e497902101c758242458c1fc93e
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-30 08:38:47 +0300

    feat: add base ProductItem widget

--- lib/features/products_overview/views/products_overview_screen.dart
+++ lib/features/products_overview/views/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../data/data.dart';
+import '../widgets/widgets.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   const ProductsOverviewScreen({super.key});
@@ -11,20 +12,23 @@ class ProductsOverviewScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Shop'),
       ),
-      body: SingleChildScrollView(
-          child: SafeArea(
+      body: SafeArea(
         child: GridView.builder(
+          itemCount: dummyProducts.length,
+          padding: const EdgeInsets.all(10),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             childAspectRatio: 3 / 2,
             crossAxisCount: 2,
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
           ),
-          itemBuilder: (context, item) => Container(),
-          itemCount: dummyProducts.length,
-          padding: const EdgeInsets.all(10),
+          itemBuilder: (context, item) => ProductItem(
+            id: dummyProducts[item].id,
+            imageUrl: dummyProducts[item].imageUrl,
+            title: dummyProducts[item].title,
+          ),
         ),
-      )),
+      ),
     );
   }
 }
