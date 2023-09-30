2ac3aeeca59976824edce3a89356e11865f6b1bf
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-30 18:37:16 +0300

    fix: remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,6 @@ class MyApp extends StatelessWidget {
       title: 'Base Store',
       debugShowCheckedModeBanner: false,
       theme: themeData(context),
-      // ThemeData(
-      //   fontFamily: 'Lato',
-      //   scaffoldBackgroundColor: const Color(0xff121418),
-      //   appBarTheme: Theme.of(context).appBarTheme.copyWith(
-      //         backgroundColor: const Color(0xff202329),
-      //         titleTextStyle: const TextStyle(
-      //           color: Color(0xffD0D0D0),
-      //         ),
-      //       ),
-      //   textTheme: Theme.of(context).textTheme.apply(
-      //         fontFamily: 'Lato',
-      //         bodyColor: const Color(0xffD0D0D0),
-      //         displayColor: const Color(0xffD0D0D0),
-      //       ),
-      //   useMaterial3: true,
-      // ),
       home: const ProductsOverviewScreen(),
     );
   }
