197077cc4e44585c70bd2c0dc477fac73035bef9
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-30 09:01:12 +0300

    feat: add colors

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Base Store',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black26),
+        scaffoldBackgroundColor: const Color(0xff121418),
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+              backgroundColor: const Color(0xff202329),
+              titleTextStyle: const TextStyle(
+                color: Color(0xffD0D0D0),
+              ),
+            ),
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: const Color(0xffD0D0D0),
+              displayColor: const Color(0xffD0D0D0),
+            ),
         useMaterial3: true,
       ),
       home: const ProductsOverviewScreen(),
