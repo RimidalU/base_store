e46da77633f8cfd1458e033c7a3ee5220c8b9509
RimidalU <stankevichuuladimiru@gmail.com>
2023-10-04 11:15:11 +0300

    feat: add totallAmount to card widget

--- lib/features/cart.dart/views/cart_screen.dart
+++ lib/features/cart.dart/views/cart_screen.dart
@@ -1,32 +1,40 @@
+import 'package:base_store/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Column(children: [
           Card(
-            margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
               child: Row(
                 children: [
-                  Text(
+                  const Text(
                     'Total',
                     style: TextStyle(
                       fontSize: 20,
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 10,
                   ),
-                  Chip(label: Text('Sum'))
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount.toString()}',
+                    ),
+                  ),
                 ],
               ),
             ),
