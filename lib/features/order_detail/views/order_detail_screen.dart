5041349197dec6dc4acbd290ba32848f149d9ac4
RimidalU <stankevichuuladimiru@gmail.com>
2023-10-05 18:02:50 +0300

    implement app_drawer widget

---
+++ lib/features/order_detail/views/order_detail_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../providers/products_provider.dart';
+
+class OrderDetailScreen extends StatelessWidget {
+  const OrderDetailScreen({super.key});
+
+  static const routeName = '/order-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    final productId = ModalRoute.of(context)?.settings.arguments as String;
+    final product = Provider.of<ProductsProvider>(context, listen: false)
+        .findById(productId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          product.title,
+          style: const TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                width: screenWidth,
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                '\$${product.price}',
+                style: const TextStyle(fontSize: 22),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: screenWidth,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  product.description,
+                  style: const TextStyle(fontSize: 22),
+                  softWrap: true,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
