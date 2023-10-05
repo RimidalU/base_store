2f98e1ecacecfd8482565facc3db760e35a4f557
RimidalU <stankevichuuladimiru@gmail.com>
2023-10-05 03:00:59 +0300

    feat: implement base product detail screen

--- lib/features/product_detail/views/product_detail_screen.dart
+++ lib/features/product_detail/views/product_detail_screen.dart
@@ -10,21 +10,53 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     final productId = ModalRoute.of(context)?.settings.arguments as String;
     final product = Provider.of<ProductsProvider>(context, listen: false)
         .findById(productId);
 
     return Scaffold(
-        appBar: AppBar(
-      title: Text(
-        product.title,
-        style: const TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        title: Text(
+          product.title,
+          style: const TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-    )
-        // ), body: SafeArea(child: )
-        );
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                width: screenWidth,
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                '\$${product.price}',
+                style: const TextStyle(fontSize: 22),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: screenWidth,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  product.description,
+                  style: const TextStyle(fontSize: 22),
+                  softWrap: true,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
