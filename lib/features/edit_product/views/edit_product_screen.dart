6a96dcb44c1f6242f267ce488bd907a475bd1a00
RimidalU <stankevichuuladimiru@gmail.com>
2023-10-07 23:36:05 +0300

    feat: implement base form

--- lib/features/edit_product/views/edit_product_screen.dart
+++ lib/features/edit_product/views/edit_product_screen.dart
@@ -10,6 +10,30 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final imageUrlController = TextEditingController();
+  final imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    imageUrlFocusNode.addListener(updateImageUrl);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    imageUrlFocusNode.removeListener(updateImageUrl);
+    imageUrlController.dispose();
+    imageUrlFocusNode.dispose();
+  }
+
+  void updateImageUrl() {
+    if (!imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +45,59 @@ class _EditProductScreenState extends State<EditProductScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+            child: ListView(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              textInputAction: TextInputAction.next,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Price'),
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.number,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Description'),
+              textInputAction: TextInputAction.next,
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: const EdgeInsets.only(top: 8, right: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        width: 1,
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
+                  child: imageUrlController.text.isEmpty
+                      ? const Center(child: Text('Inter a Url'))
+                      : FittedBox(
+                          fit: BoxFit.cover,
+                          clipBehavior: Clip.hardEdge,
+                          child: Image.network(imageUrlController.text),
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: 'Image URL'),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: imageUrlController,
+                    focusNode: imageUrlFocusNode,
+                  ),
+                ),
+              ],
+            )
+          ],
+        )),
       ),
     );
   }
