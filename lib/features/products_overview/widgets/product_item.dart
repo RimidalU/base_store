3f0b3ac6169f48dbaca2d95c0762479663149722
RimidalU <stankevichuuladimiru@gmail.com>
2023-10-07 18:16:42 +0300

    feat: add SnackBar widget with removeSingleProduct metod to addProductToCart action

--- lib/features/products_overview/widgets/product_item.dart
+++ lib/features/products_overview/widgets/product_item.dart
@@ -41,6 +41,21 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_bag),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Product added to cart!',
+                  ),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleProduct(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).indicatorColor,
           ),
