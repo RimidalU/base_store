ac0828fe07666fe573630bfa48941a2be5dc3d10
RimidalU <stankevichuuladimiru@gmail.com>
2023-10-07 16:27:59 +0300

    feat: implement list of products in orders list

--- lib/features/orders_overview/widgets/order_item.dart
+++ lib/features/orders_overview/widgets/order_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../../../providers/providers.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   const OrderItem({
     super.key,
     required this.order,
@@ -12,24 +14,70 @@ class OrderItem extends StatelessWidget {
   final ord.OrderItem order;
 
   @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var expended = false;
+
+  double getHeight() {
+    return widget.order.products.length * 20 + 10;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10),
-      child: Column(children: [
-        ListTile(
-          title: Text(
-            '\$${order.amount}',
-            style: const TextStyle(fontSize: 20),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              '\$${widget.order.amount}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            subtitle: Text(
+              DateFormat('dd/MM/yyy   HH:mm').format(widget.order.createAt),
+            ),
+            trailing: IconButton(
+              icon: Icon(expended ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  expended = !expended;
+                });
+              },
+            ),
           ),
-          subtitle: Text(
-            DateFormat('dd/MM/yyy   HH:mm').format(order.createAt),
-          ),
-          trailing: IconButton(
-            icon: const Icon(Icons.expand_more),
-            onPressed: () {},
-          ),
-        )
-      ]),
+          if (expended)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: min(getHeight(), 180),
+              child: ListView.builder(
+                itemBuilder: (
+                  context,
+                  item,
+                ) =>
+                    Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.order.products[item].title,
+                      style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w200,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    Text(
+                        '${widget.order.products[item].quantity} x \$${widget.order.products[item].price}')
+                  ],
+                ),
+                itemCount: widget.order.products.length,
+              ),
+            )
+        ],
+      ),
     );
   }
 }
