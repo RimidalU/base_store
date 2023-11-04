91f7104e2b384a2232e653b6ae1dcb09dc81bbe0
RimidalU <stankevichuuladimiru@gmail.com>
2023-11-04 13:32:36 +0300

    feat: implement load products from  server

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -4,11 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:uuid/uuid.dart';
 
-import './data/fake_products.dart';
 import '../providers/providers.dart';
 
 class ProductsProvider with ChangeNotifier {
-  List<ProductProvider> items = dummyProducts;
+  List<ProductProvider> items = [];
   var uuid = const Uuid();
 
   List<ProductProvider> get products {
@@ -21,6 +20,36 @@ class ProductsProvider with ChangeNotifier {
 
   ProductProvider findById(String id) {
     return items.firstWhere((item) => item.id == id);
+  }
+
+  Future<void> fetchAndSetProducts() async {
+    const url =
+        'https://base-store-e0c1b-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+      );
+
+      final extractData = jsonDecode(response.body) as Map<String, dynamic>;
+
+      final List<ProductProvider> loadedProducts = [];
+
+      extractData.forEach((prodId, prodData) {
+        loadedProducts.add(ProductProvider(
+          id: prodId,
+          title: prodData['title'],
+          description: prodData['description'],
+          price: prodData['price'],
+          imageUrl: prodData['imageUrl'],
+          isFavorite: prodData['isFavorite'],
+        ));
+      });
+      items = loadedProducts;
+      notifyListeners();
+    } catch (error) {
+      rethrow;
+    }
   }
 
   Future<void> addProduct(ProductProvider product) async {
